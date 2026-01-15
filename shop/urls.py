80491bd5ed803b6d567d1b2216e3cc85dedd9f1b
Ariovistus <maxgooz@yandex.ru>
2026-01-15 19:20:19 +0300

    DEBUG

--- shop/urls.py
+++ shop/urls.py
@@ -4,14 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('users/', include('users.urls', namespace='users')),
-    path('orders/', include('orders.urls', namespace='orders')),
-    path('payment/', include('payment.urls', namespace='payment')),
+    path("admin/", admin.site.urls),
     path('', include('main.urls', namespace='main')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    
