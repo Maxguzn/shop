0b082620623917d0281885078eb44625dc2acaba
Ariovistus <maxgooz@yandex.ru>
2026-01-19 23:56:25 +0300

    Добавили приложение users(forms, views, urrls, models)

--- shop/urls.py
+++ shop/urls.py
@@ -6,10 +6,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('users/', include('users.urls', namespace='users')),
     path('', include('main.urls', namespace='main')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
+    urlpatterns  += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
     
